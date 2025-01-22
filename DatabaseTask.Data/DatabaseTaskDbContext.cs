e5f8865c7bb979bd31440c5fb688940d2dc46932
Allan Lond <158562268+Jamarare@users.noreply.github.com>
2025-01-22 12:19:22 +0200

    .

---
+++ DatabaseTask.Data/DatabaseTaskDbContext.cs
@@ -0,0 +1,22 @@
+﻿using DatabaseTask.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DatabaseTask.Data
+{
+    public class DatabaseTaskDbContext : DbContext
+    {
+        public DatabaseTaskDbContext()
+        {
+        }
+
+        public DatabaseTaskDbContext(DbContextOptions<DatabaseTaskDbContext> options)
+            : base(options) { }
+
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<Project> Project { get; set; }
+        public DbSet<Specialist> Specialist { get; set; }
+        public DbSet<ProjectEmployee> ProjectEmployee { get; set; }
+        public DbSet<ProjectSpecialist> ProjectSpecialist { get; set; }
+    }
+}
---
+++ DatabaseTask.Data/Migrations/20250108131831_migration1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DatabaseTask.Data.Migrations
+{
+    public partial class migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Employee",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Employee");
+
+            migrationBuilder.AddColumn<int>(
+                name: "EmployeeID",
+                table: "Employee",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Employee",
+                table: "Employee",
+                column: "EmployeeID");
+
+            migrationBuilder.CreateTable(
+                name: "Project",
+                columns: table => new
+                {
+                    ProjectID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Project", x => x.ProjectID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjectEmployee",
+                columns: table => new
+                {
+                    ProjectID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectEmployee", x => x.ProjectID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjectSpecialist",
+                columns: table => new
+                {
+                    ProjectID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SpecialistID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectSpecialist", x => x.ProjectID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Specialist",
+                columns: table => new
+                {
+                    SpecialistID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Specialist", x => x.SpecialistID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Project");
+
+            migrationBuilder.DropTable(
+                name: "ProjectEmployee");
+
+            migrationBuilder.DropTable(
+                name: "ProjectSpecialist");
+
+            migrationBuilder.DropTable(
+                name: "Specialist");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Employee",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeID",
+                table: "Employee");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                table: "Employee",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Employee",
+                table: "Employee",
+                column: "Id");
+        }
+    }
+}
